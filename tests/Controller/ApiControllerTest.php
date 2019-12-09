54f5e031085150232f98eefb1e5fa7e940bbabd7
Mponos George <gmponos@gmail.com>
2019-12-09 20:12:47 +0200

    Added extra tests

--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -12,5 +12,15 @@ class ApiControllerTest extends WebTestCase
         $client->request('post', '/api');
         $this->assertSame(405, $client->getResponse()->getStatusCode());
         $this->assertSame('application/json', $client->getResponse()->headers->get('Content-type'));
+        $this->assertJson($client->getResponse()->getContent());
+    }
+
+    public function testApiWithDebugFalse()
+    {
+        $client = static::createClient(['debug' => false]);
+        $client->request('post', '/api');
+        $this->assertSame(405, $client->getResponse()->getStatusCode());
+        $this->assertSame('application/json', $client->getResponse()->headers->get('Content-type'));
+        $this->assertJson($client->getResponse()->getContent());
     }
 }
